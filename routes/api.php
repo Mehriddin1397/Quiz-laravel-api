b55bc902dd54d0f1a42714111cdec79c5b5885cb
Mehriddin1397 <soyibovmehriddin@gmail.com>
2024-07-06 20:25:06 +0500

    create Quiz, question and answer Controller

--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 
-Route::get('/hello', function () {
-    return response()->json(['message' => 'Hello, World!']);
-});
+Route::apiResource('quizzes', \App\Http\Controllers\Api\QuizController::class);
+
